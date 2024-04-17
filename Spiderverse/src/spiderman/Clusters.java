f020a46b8d5d576d593b8730fbe51a9f0b9230f4
DAmin05 <devhamin982@gmail.com>
2024-04-17 15:44:49 -0400

    clusters

--- Spiderverse/src/spiderman/Clusters.java
+++ Spiderverse/src/spiderman/Clusters.java
@@ -1,5 +1,7 @@
 package spiderman;
 
+import java.util.*;
+
 /**
  * Steps to implement this class main method:
  * 
@@ -23,6 +25,7 @@ package spiderman;
 
 public class Clusters {
 
+    private HashMap<Integer , ArrayList<Integer>> clusters;
     public static void main(String[] args) {
 
         if ( args.length < 2 ) {
@@ -32,6 +35,80 @@ public class Clusters {
         }
 
         // WRITE YOUR CODE HERE
+        Clusters c = new Clusters();
+        c.createHashMap(args[0]);
+        c.printing(args[1]);
 
+    }
+
+    private void createHashMap(String fileName)
+    {
+        StdIn.setFile(fileName);
+        int a_numDimension = StdIn.readInt();
+        int b_hashSize = StdIn.readInt();
+        double c_threshold = StdIn.readDouble();
+
+        clusters = new HashMap<>(b_hashSize);
+
+        int dimAdded = 0;
+        
+        for(int i=0 ; i<a_numDimension ; i++)
+        {
+            int dimension = StdIn.readInt();
+            int canonEvent = StdIn.readInt();
+            int dimWeight = StdIn.readInt();
+
+            if(!clusters.containsKey(dimension % b_hashSize))
+            {
+                clusters.put(dimension % b_hashSize , new ArrayList<>());
+            }
+            clusters.get(dimension % b_hashSize).add(0 , dimension);
+            dimAdded++;
+
+            if((double)dimAdded/clusters.size() >= c_threshold)
+            {   
+                int size = clusters.size()*2;
+                rehashing(size);
+            }
+
+            connection();
+        }
+    }
+
+    private void rehashing(int size)
+    {
+        HashMap<Integer , ArrayList<Integer>> temp = new HashMap<>(size);
+        
+        for(ArrayList<Integer> list : clusters.values())
+        {
+            for(int dimension : list)
+            {
+                if(!temp.containsKey(dimension % size))
+                {
+                    temp.put(dimension % size , new ArrayList<>());
+                }
+                temp.get(dimension % size).add(0 , dimension);
+            }
+        }
+        clusters.clear();
+        clusters.putAll(temp);
+    }
+
+    private void connection()
+    {
+        return;
+    }
+
+    private void printing(String fileName)
+    {
+        //StdOut.setFile(fileName);
+        for(ArrayList<Integer> list : clusters.values())
+        {
+            for(int dimension : list)
+            {
+                System.out.print(dimension + " ");
+            }
+            System.out.println();
+        }
     }
 }
